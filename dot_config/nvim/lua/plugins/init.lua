3cd14e31fa7282295c8e9cc8076803808b091c82
Martin Irigaray <martin.irigaray@ideati.net>
2026-05-19 14:08:59 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -15,16 +15,26 @@ return {
     },
   },
 
-  { "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "bash", "json", "yaml", "markdown" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+{
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "bash",
+        "json",
+        "yaml",
+        "markdown",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+},
 
   { "lewis6991/gitsigns.nvim", config = true },
 
